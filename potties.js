ad19cfbd391c2641adb9f4b98c1060d7697e0470
Brad Said <bradsaid@gmail.com>
2018-08-19 10:30:40 -0500

    DB corrections

--- potties.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  let Potties = sequelize.define("Potties", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Potties;
-};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,45 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+module.exports = function (app) {
+  // Get all examples (test DB)
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
+
+  // Potties DB 
+  app.get("/api/potties", function (req, res) {
+    db.Potties.findAll({}).then(function (potties_db) {
+      res.json(potties_db);
+    });
+  });
+
+  app.post("/api/potties", function (req, res) {
+    db.Potties.create(req.body).then(function (potties_db) {
+      res.json(potties_db);
+    });
+  });
+
+  app.delete("/api/potties/:id", function (req, res) {
+    db.Potties.destroy({ where: { id: req.params.id } }).then(function (potties_db) {
+      res.json(potties_db);
+    });
+  });
+
+
 };
