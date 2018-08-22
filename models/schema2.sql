6204a31d7b98ef38d6372cf7617b16c2d991f138
SymPlee Rich <richard.dorleans@yahoo.com>
2018-08-21 19:37:45 -0500

    The potties schema updated with second table

--- models/schema2.sql
+++ models/schema2.sql
@@ -4,6 +4,7 @@ USE potties_db;
 
 CREATE TABLE categories
 (
+    potties_id int NOT NULL AUTO_INCREMENT,    
     name varchar(55) NOT NULL,
     addr varchar(55) NOT NULL,
     longitude varchar(20),
@@ -15,5 +16,15 @@ CREATE TABLE categories
     ChangeTable BOOLEAN,
     Unisex BOOLEAN,
     keyRequired BOOLEAN,
-    picLink varchar(99)
+    picLink varchar(99),
+    PRIMARY KEY (potties_id)
 );
+
+CREATE TABLE ratingsAvg
+(
+	ratings_id int NOT NULL AUTO_INCREMENT,
+    potties_id int NOT NULL,
+    cleanRating INT(1) NOT NULL,
+    PRIMARY KEY (ratings_id),
+    FOREIGN KEY (potties_id) REFERENCES categories(potties_id)
+    );
