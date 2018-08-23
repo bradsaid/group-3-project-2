6f74d4761b3a4ab664f4e4e28b150a3d79ac66d5
SymPlee Rich <richard.dorleans@yahoo.com>
2018-08-23 18:30:30 -0500

    Ratings average thru sequelize, still needs work

--- models/schema2.sql
+++ models/schema2.sql
@@ -4,7 +4,6 @@ USE potties_db;
 
 CREATE TABLE categories
 (
-    potties_id int NOT NULL AUTO_INCREMENT,    
     name varchar(55) NOT NULL,
     addr varchar(55) NOT NULL,
     longitude varchar(20),
@@ -17,14 +16,4 @@ CREATE TABLE categories
     Unisex BOOLEAN,
     keyRequired BOOLEAN,
     picLink varchar(99),
-    PRIMARY KEY (potties_id)
 );
-
-CREATE TABLE ratingsAvg
-(
-	ratings_id int NOT NULL AUTO_INCREMENT,
-    potties_id int NOT NULL,
-    cleanRating INT(1) NOT NULL,
-    PRIMARY KEY (ratings_id),
-    FOREIGN KEY (potties_id) REFERENCES categories(potties_id)
-    );
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,48 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Get all examples (test DB)
-  app.get("/api/examples", function (req, res) {
+  app.get("/api/examples", function(req, res) {
     db.Potties.findAll({}).then(function (dbPotties) {
       res.json(dbPotties);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Potties.create(req.body).then(function (dbExample) {
+  app.post("/api/examples", function(req, res) {
+    db.Potties.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Potties.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Potties.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Potties DB 
-  app.get("/api/potties", function (req, res) {
-    db.Potties.findAll({}).then(function (potties_db) {
-      res.json(potties_db);
+  app.get("/api/potties", function(req, res) {
+    db.Potties.findAll({
+      attributes: [
+        [sequelize.fn("AVG", sequelize.col("cleanliness")), "cleanAvg"]
+      ]
+    }).then(function(dbPotties) {
+      res.json(dbPotties);
+      console.log(cleanAvg);
     });
   });
 
-  app.post("/api/potties", function (req, res) {
-    db.Potties.create(req.body).then(function (potties_db) {
-      res.json(potties_db);
+  app.post("/api/potties", function(req, res) {
+    db.Potties.create(req.body).then(function(dbPotties) {
+      res.json(dbPotties);
     });
   });
 
-  app.delete("/api/potties/:id", function (req, res) {
-    db.Potties.destroy({ where: { id: req.params.id } }).then(function (potties_db) {
-      res.json(potties_db);
+  app.delete("/api/potties/:id", function(req, res) {
+    db.Potties.destroy({ where: { id: req.params.id } }).then(function(dbPotties) {
+      res.json(dbPotties);
     });
   });
-
-
 };
